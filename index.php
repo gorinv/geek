04d6363b2eb52da9bc620187116a1c6437a27faf
gorinv <60851936+gorinv@users.noreply.github.com>
2020-02-09 22:22:21 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="" ~~en~~ru "">
+<?  
+$title='Lorem'; 
+$h1='<h1> This is h1</h1>';
+$year=2020;
+?>
+
+
+
+
+<head>
+    <meta charset="UTF-8">
+    <title><? echo $title;?></title></head>
+
+<body>
+<? echo $h1; ?>
+
+<? echo $year.'© Все права защищены'; ?>
+</body>
+
+</html>
